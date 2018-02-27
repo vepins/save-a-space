170eb1ba6e4f4ad1dd0bbdad8504c7daad454bf5
Jason Byer <jbyer3@gmail.com>
2018-02-27 13:34:27 -0500

    added join table for users and reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,6 +10,7 @@ class ReservationsController < ApplicationController
   # GET /reservations/1
   # GET /reservations/1.json
   def show
+    @reservations = Reservation.all
   end
 
   # GET /reservations/new
